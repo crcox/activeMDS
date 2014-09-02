70524dce1db9dcf7c66b7feba1b53a793fad6e00
Chris Cox <crcox@wisc.edu>
2014-09-02 14:05:45 -0500

    sync

--- py/activeMDS/activeMDS.py
+++ py/activeMDS/activeMDS.py
@@ -1,13 +1,32 @@
 import unittest
 import numpy
 
+class Gradient:
+    def __init__(self):
+        self.g = None
+        self.eloss = None
+        self.hloss = None
+
 def modelCheck(model, query):
     d1 = numpy.sqrt(numpy.sum(numpy.square(model[query[0],] - model[query[2],])))
     d2 = numpy.sqrt(numpy.sum(numpy.square(model[query[1],] - model[query[2],])))
     return(d1<d2)
 
 def updateModel(model, query):
-    
+    def StochasticGradient(model, query):
+        H = mat([[1.,0.,-1.],[ 0.,  -1.,  1.],[ -1.,  1.,  0.]])
+        dims = numpy.ndarray.shape(model)[1] # number of dimensions
+        X = model[query, ]
+        gradient = Gradient()
+        gradient.g = zeros((3,dims))
+        gradient.eloss = 0
+        gradient.hloss = 0
+
+
+
+
+    gradient = StochasticGradient()
+
 
 class FooTests(unittest.TestCase):
     def testModelIsCorrect(self):
